42d86eae30f9e1f60af368e365b3338df16d8e40
mlfc <mlfc0422@outlook.com>
2024-05-21 12:56:08 +0800

    个人页面加入饼图

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,12 @@ import ElementPlus from 'element-plus'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import '../src/utils/axios-config.js'
+import * as echarts from 'echarts';
 
 
 const app = createApp(App)
+// 将 ECharts 添加到全局属性中
+app.config.globalProperties.$echarts = echarts;
 app.use(ElementPlus)
 app.use(router)
 app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,6 @@
       <el-descriptions-item label="身高">{{ userData.height }}</el-descriptions-item>
       <el-descriptions-item label="体重">{{ userData.weight }}</el-descriptions-item>
 
-
       <el-descriptions-item label="操作" style="text-align: center;">
         <el-button plain @click="dialogFormVisible = true">修改个人信息</el-button>
       </el-descriptions-item>
@@ -45,51 +44,105 @@
         </div>
       </template>
     </el-dialog>
+
+    <div ref="chartRef" style="width: 400px; height: 400px;"></div>
   </div>
 </template>
 
 <script setup lang="ts">
-
-import {onMounted, ref} from "vue";
-import axios from "axios";
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import * as echarts from 'echarts';
 
 const userData = ref({
   username: '',
   password: '',
-  name:'',
+  name: '',
   phone: '',
   age: '',
   height: '',
   weight: '',
 });
-const formLabelWidth = '140px'
-const dialogFormVisible = ref(false)
-onMounted(onPageLoaded);
 
-function onPageLoaded() {
-  axios.get('user')
-      .then(res => {
-        userData.value = res.data.data;
-      })
-      .catch(err => {
-        console.log(err);
-      });
-}
+const CourseCount = ref([]);
 
-function handleConfirm() {
-  axios.put('user', userData.value)
-      .then(res => {
-        if (res.data.code === 1) {
-          alert('修改成功');
-        } else {
-          alert('修改失败');
-        }
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  dialogFormVisible.value = false
-}
+const formLabelWidth = '140px';
+const dialogFormVisible = ref(false);
+const chartRef = ref<HTMLElement | null>(null);
+
+const fetchData = async () => {
+  try {
+    const res = await axios.get('user');
+    userData.value = res.data.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const initChart = () => {
+  if (chartRef.value) {
+    const myChart = echarts.init(chartRef.value);
+    const option = {
+      title: {
+        text: '课程分析',
+        left: 'center',
+      },
+      tooltip: {
+        trigger: 'item',
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left',
+      },
+      series: [
+        {
+          name: '数据',
+          type: 'pie',
+          radius: '50%',
+          data: CourseCount.value.map(item => ({ value: item.courseCount, name: item.courseName })),
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)',
+            },
+          },
+        },
+      ],
+    };
+    myChart.setOption(option);
+  }
+};
+
+const myCourseCount = async () => {
+  try {
+    const res = await axios.get('course/myCourseCount');
+    CourseCount.value = res.data.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const handleConfirm = async () => {
+  try {
+    const res = await axios.put('user', userData.value);
+    if (res.data.code === 1) {
+      alert('修改成功');
+    } else {
+      alert('修改失败');
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  dialogFormVisible.value = false;
+};
+
+onMounted(async () => {
+  await fetchData();
+  await myCourseCount();
+  initChart();
+});
+
 </script>
 
 <style scoped>
