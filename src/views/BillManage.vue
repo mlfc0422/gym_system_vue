2cf7ab746c547f3f1694942e7169a6c345d9bb51
mlfc <mlfc0422@outlook.com>
2024-06-01 14:04:28 +0800

    账单管理修改

--- src/views/BillManage.vue
+++ src/views/BillManage.vue
@@ -15,6 +15,8 @@ const billList = ref<Bill[]>([]);
 const loading = ref(false);
 const currentBill = ref<Partial<Bill>>({});
 const showAddBillDialog = ref(false);
+const isEdit = ref(false);
+
 onMounted(() => {
   getBillList();
 });
@@ -52,6 +54,29 @@ function addBill() {
         console.error('添加账单请求失败:', error);
       });
 }
+
+function editBill(bill: Bill) {
+  isEdit.value = true;
+  currentBill.value = {...bill};
+  showAddBillDialog.value = true;
+}
+
+function updateBill() {
+  axios.put(`/bill`, currentBill.value)
+      .then(res => {
+        if (res.data.code === 1) {
+          getBillList();
+          showAddBillDialog.value = false;
+          currentBill.value = {};
+          isEdit.value = false;
+        } else {
+          alert('更新账单失败: ' + res.data.msg);
+        }
+      })
+      .catch(error => {
+        console.error('更新账单请求失败:', error);
+      });
+}
 </script>
 
 <template>
@@ -77,27 +102,36 @@ function addBill() {
           <el-table-column prop="amount" label="总金额" width="180"></el-table-column>
           <el-table-column prop="createTime" label="创建日期" width="200"></el-table-column>
           <el-table-column prop="updateTime" label="修改日期" width="200"></el-table-column>
+          <el-table-column label="操作" width="150">
+            <template #default="scope">
+              <el-button @click="editBill(scope.row)" type="primary" size="small">编辑</el-button>
+            </template>
+          </el-table-column>
         </el-table>
 
-        <!-- 新增账单对话框 -->
-        <el-dialog title="新增账单" v-model="showAddBillDialog">
+        <!-- 新增/编辑账单对话框 -->
+        <el-dialog :title="isEdit ? '编辑账单' : '新增账单'" v-model="showAddBillDialog">
           <el-form :model="currentBill">
             <el-form-item label="描述">
-              <el-input v-model="currentBill.text"></el-input>
+              <el-input v-model="currentBill.text" :disabled="!isEdit"></el-input>
             </el-form-item>
             <el-form-item label="金额">
-              <el-input v-model="currentBill.amount" type="number"></el-input>
+              <el-input v-model="currentBill.amount" type="number" :disabled="isEdit"></el-input>
             </el-form-item>
           </el-form>
           <div slot="footer" class="dialog-footer">
             <el-button @click="showAddBillDialog = false">取消</el-button>
-            <el-button type="primary" @click="addBill">添加</el-button>
+            <el-button type="primary" @click="isEdit ? updateBill() : addBill">{{
+                isEdit ? '保存' : '添加'
+              }}
+            </el-button>
           </div>
         </el-dialog>
       </el-main>
     </el-container>
   </div>
 </template>
+
 
 <style scoped>
 
